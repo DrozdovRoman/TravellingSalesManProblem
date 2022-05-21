33c1664694c1b8e65a9ccc3fe3957a984d9ebadc
DrozdovRoman <91407365+DrozdovRoman@users.noreply.github.com>
2022-05-21 15:35:24 +0500

    Update main.cpp

--- TravellingSalesManProblem/main.cpp
+++ TravellingSalesManProblem/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    MainWindow w; // инициализация переменной класса MainWindow
     w.show();
     return a.exec();
 }
